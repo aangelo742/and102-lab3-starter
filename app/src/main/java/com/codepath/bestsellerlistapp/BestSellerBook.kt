5c990dd11163990f15686bbae9c2dc4ce0fe2ca5
aangelo742 <83144033+aangelo742@users.noreply.github.com>
2022-09-24 16:09:46 -0500

    Step 3

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBook.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBook.kt
@@ -1,6 +1,8 @@
 package com.codepath.bestsellerlistapp
 
+import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
+import com.google.gson.reflect.TypeToken
 
 /**
  * The Model for storing a single book from the NY Times API
@@ -21,10 +23,12 @@ class BestSellerBook {
     var author: String? = null
 
     //TODO bookImageUrl
-
+    @SerializedName("book_image")
+    var bookImageUrl: String? = null
 
     //TODO description
-
+    @SerializedName("description")
+    var description: String? = null
 
     //TODO-STRETCH-GOALS amazonUrl
 }
